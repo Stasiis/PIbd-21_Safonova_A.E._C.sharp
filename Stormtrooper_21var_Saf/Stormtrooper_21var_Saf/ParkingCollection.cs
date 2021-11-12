90476390eeb911d56752f96f090a3ff9b6ec1bc1
Stasiis <90193260+Stasiis@users.noreply.github.com>
2021-11-12 13:30:06 +0400

    Add files via upload

---
+++ Stormtrooper_21var_Saf/Stormtrooper_21var_Saf/ParkingCollection.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsPlanes;
+
+namespace WindowsFormsParking
+{
+    public class ParkingCollection
+    {
+        public readonly Dictionary<string, Parking<Vehicle>> parkingStages;
+        public List<string> Keys => parkingStages.Keys.ToList();
+        //
+
+        //
+        private readonly int pictureWidth;
+        private readonly int pictureHeight;
+        public ParkingCollection(int pictureWidth, int pictureHeight)
+        {
+            parkingStages = new Dictionary<string, Parking<Vehicle>>();
+            this.pictureWidth = pictureWidth;
+            this.pictureHeight = pictureHeight;
+        }
+        public void AddParking(string name)
+        {
+            if (parkingStages.ContainsKey(name))
+            {
+                return;
+            }
+            parkingStages.Add(name, new Parking<Vehicle>(pictureHeight, pictureWidth));
+        }
+        /// <summary>
+        /// Удаление парковки
+        /// </summary>
+        /// <param name="name">Название парковки</param>
+        public void DelParking(string name)
+        {
+            if (parkingStages.ContainsKey(name))
+            {
+                parkingStages.Remove(name);
+            }
+        }
+        /// <summary>
+        /// Доступ к парковке
+        /// </summary>
+        /// <param name="ind"></param>
+        /// <returns></returns>
+        public Parking<Vehicle> this[string ind]
+        {
+            get
+            {
+                if (parkingStages.ContainsKey(ind))
+                {
+                    return parkingStages[ind];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
