3abf387768e3f31f00266e3021f5d656ff9746da
Stasiis <90193260+Stasiis@users.noreply.github.com>
2021-11-27 14:26:01 +0400

    Update and rename ParkingCollection.cs to HangarCollection.cs

---
+++ Stormtrooper_21var_Saf/Stormtrooper_21var_Saf/HangarCollection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsPlanes;
+
+namespace Stormtrooper_21var_Saf
+{
+    public class HangarCollection
+    {
+        public readonly Dictionary<string, Hangar<Vehicle>> HangarStages;
+        public List<string> Keys => HangarStages.Keys.ToList();
+        private readonly int pictureWidth;
+        private readonly int pictureHeight;
+        public HangarCollection(int pictureWidth, int pictureHeight)
+        {
+            HangarStages = new Dictionary<string, Hangar<Vehicle>>();
+            this.pictureWidth = pictureWidth;
+            this.pictureHeight = pictureHeight;
+        }
+        public void AddHangar(string name)
+        {
+            if (HangarStages.ContainsKey(name))
+            {
+                return;
+            }
+            HangarStages.Add(name, new Hangar<Vehicle>(pictureHeight, pictureWidth));
+        }
+        public void DelHangar(string name)
+        {
+            if (HangarStages.ContainsKey(name))
+            {
+                HangarStages.Remove(name);
+            }
+        }
+        public Hangar<Vehicle> this[string ind]
+        {
+            get
+            {
+                if (HangarStages.ContainsKey(ind))
+                {
+                    return HangarStages[ind];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
