12eed5d8a1f04d021df043b66217d797b9935ba5
Stasiis <90193260+Stasiis@users.noreply.github.com>
2021-11-27 15:00:44 +0400

    Update Stormtrooper.cs

--- Stormtrooper_21var_Saf/Stormtrooper_21var_Saf/Stormtrooper.cs
+++ Stormtrooper_21var_Saf/Stormtrooper_21var_Saf/Stormtrooper.cs
@@ -28,6 +28,19 @@ namespace Stormtrooper_21var_Saf
             Rockets = rockets;
             Window = window;
         }
+        public Stormtrooper(string info) : base(info)
+        {
+            string[] strs = info.Split(separator);
+            if (strs.Length == 6)
+            {
+                MaxSpeed = Convert.ToInt32(strs[0]);
+                Weight = Convert.ToInt32(strs[1]);
+                MainColor = Color.FromName(strs[2]);
+                DopColor = Color.FromName(strs[3]);
+                Rockets = Convert.ToBoolean(strs[4]);
+                Window = Convert.ToBoolean(strs[5]);
+            }
+        }
         public override void DrawTransport(Graphics g)
         {
             Pen pen = new Pen(Color.Black);
@@ -50,6 +63,10 @@ namespace Stormtrooper_21var_Saf
         public void SetDopColor(Color color)
         {
             DopColor = color;
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}{separator}{DopColor.Name}{separator}{Rockets}{separator}{Window}";
         }
     }
 }
