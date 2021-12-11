0a24e5d91b937e66abece1c0f034dd26ddc10c83
Stasiis <90193260+Stasiis@users.noreply.github.com>
2021-12-11 23:36:15 +0400

    Update PlaneComparer.cs

--- Stormtrooper_21var_Saf/Stormtrooper_21var_Saf/PlaneComparer.cs
+++ Stormtrooper_21var_Saf/Stormtrooper_21var_Saf/PlaneComparer.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using WindowsFormsPlanes;
 
-namespace WindowsFormsParking
+namespace Stormtrooper_21var_Saf
 {
     public class PlaneComparer : IComparer<Vehicle>
     {
