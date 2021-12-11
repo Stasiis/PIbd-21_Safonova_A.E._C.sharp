a73433f5e81a4b9a2ff960ba643510be870de538
Stasiis <90193260+Stasiis@users.noreply.github.com>
2021-12-11 23:18:06 +0400

    Update HangarOverflowException.cs

--- Stormtrooper_21var_Saf/Stormtrooper_21var_Saf/HangarOverflowException.cs
+++ Stormtrooper_21var_Saf/Stormtrooper_21var_Saf/HangarOverflowException.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WindowsFormsParking
+namespace Stormtrooper_21var_Saf
 {
     public class HangarOverflowException : Exception
     {
