155f70463937cf0c4daf707a46a1377802af89d1
Stasiis <90193260+Stasiis@users.noreply.github.com>
2021-12-11 23:16:53 +0400

    Add files via upload

---
+++ Stormtrooper_21var_Saf/Stormtrooper_21var_Saf/HangarOccupiedPlaceException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsParking
+{
+    class HangarOccupiedPlaceException : Exception
+    {
+        public HangarOccupiedPlaceException() : base("Место занято")
+        { }
+    }
+}
